5adecf91ada8ebfbfcb8f344c32ea55bf361631f
MesfinGithinji <meshgithinji@gmail.com>
2023-10-06 19:02:37 +0300

    Select some Amenities to be comfortable--lets make the filters section dynamic

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -21,7 +21,7 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/0-hbnb/', strict_slashes=False)
+@app.route('/1-hbnb/', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -39,7 +39,7 @@ def hbnb():
 
     cache_id = str(uuid4())
 
-    return render_template('0-hbnb.html',
+    return render_template('1-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
